8b9535ca676a01db956dbedb24b36e42d40abef5
Evil_Lu <bigshjt.evillu@gmail.com>
2019-05-26 16:46:24 +0700

    -fix ui

--- Assets/Game/Scripts/UnityScreenScriptable.cs
+++ Assets/Game/Scripts/UnityScreenScriptable.cs
@@ -3,10 +3,11 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "ScreenData", menuName = "Game UI/Screen Data", order = 0)]
 public class UnityScreenScriptable : ScriptableObject, IScreenData
 {
+    [SerializeField] string id;
     [SerializeField] GameObject screenPrefab = null;
     [SerializeField] bool isBlockable = false;
 
-    public string Id => screenPrefab.name;
+    public string Id => id;
 
     public bool IsBlockable => isBlockable;
 
